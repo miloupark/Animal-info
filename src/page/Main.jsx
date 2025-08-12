0e72dcac9e7a169ee13f8f9e8999f704d0070ff9
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-12 13:08:33 +0900

    feat(main): render animal list from data.js

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,5 +1,16 @@
+import { data } from "../assets/data/data";
+
 function Main() {
-  return <>메인 페이지</>;
+  return (
+    <ul>
+      {data.map((el) => (
+        <li key={el.id}>
+          <img src={el.img} alt={el.name} />
+          <div>{el.name}</div>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Main;
