9430d885ba76f80866cd918a44da54ea76162eca
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-12 14:02:07 +0900

    feat(search): add navigation to search page with query parameter using useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Main from "./page/Main";
 import Detail from "./page/Detail";
 import Search from "./page/Search";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
+
   return (
     <>
       <header>
         <h1>💚 동물 조아 💚</h1>
+        <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
+        <button onClick={() => navigate(`/search?animal=${inputValue}`)}>검색</button>
       </header>
       <Routes>
         <Route path="/" element={<Main />}></Route>
